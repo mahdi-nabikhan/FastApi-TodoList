22414fc7f4542949a77f7631c0882d1927be5e30
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-04-29 12:12:56 +0330

    add router for get all users from a simple list  of python

--- core/main.py
+++ core/main.py
@@ -3,7 +3,18 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+name_list = [
+    {'id':1,'name':'mmd'},
+    {'id':2,'name':'ali'},
+    {'id':3,'name':'amir'},
+    {'id':4,'name':'mosen'},
+    {'id':5,'name':'meti'},
+]
+
 @app.get('/')
 def root():
     return {'massage':" Hello World! "}
 
+@app.get('/names')
+def retrieve_names_list():
+    return name_list
