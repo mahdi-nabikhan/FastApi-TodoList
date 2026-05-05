3286d252f2ad3bca71918a2574b5b1a766db4f26
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-05-05 13:44:01 +0330

    add filed validation in BaseModelPersonSchema

--- core/schemas.py
+++ core/schemas.py
@@ -1,8 +1,17 @@
-from pydantic import BaseModel,Field
+from pydantic import BaseModel,field_validator
 
 
 class BaseModelPersonSchema(BaseModel):
+    
+    
     name:str
+    @field_validator(name)
+    def validation_name(cls,value):
+        if len(value > 32):
+            raise ValueError('name must not exceed 32 character')
+        if value.isalpha():
+            raise ValueError('name must only alphabeti character')
+        return value
 class PersonCreateSchema(BaseModelPersonSchema):
     
     age:int
