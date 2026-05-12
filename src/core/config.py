edc7034f0eb5f44f354a516060d93ce51748dfb9
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-05-12 17:26:16 +0330

    ++ add model for tasks ** refactor alembic  and make new way for importing data base to project ** refactor config.py for get sql url from .env **refactor database.py importing data from core.config

--- src/core/config.py
+++ src/core/config.py
@@ -1,9 +1,9 @@
 from pydantic_settings import BaseSettings,SettingsConfigDict
-
+from pathlib import Path
 
 class Settings (BaseSettings):
     SQLALCHEMY_DATABASE_URL :str
-    model_config = SettingsConfigDict(env_file='.env')
+    model_config = SettingsConfigDict(env_file=Path(__file__).parent/'.env')
     
     
     
--- src/core/database.py
+++ src/core/database.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine,Column,Integer,String,ForeignKey
-from config import setting
+from core.config import setting
 from sqlalchemy.orm import sessionmaker,declarative_base
 from sqlalchemy.orm import Session
 
