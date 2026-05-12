1ba066b7ae17fde1df6af7bdef372fe5a9ad925d
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-05-12 17:41:54 +0330

    +add Task schemas (for create updating and response ) in task/schemas.py

---
+++ src/tasks/schemas.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel ,Field
+from typing import Optional
+
+from datetime import datetime
+class TaskBaseSchemas (BaseModel):
+    title : str = Field(...,max_length=150,min_length=5,description='Title of The Task')
+    description : Optional[str] = Field(...,max_length=500,description='description of Task,')
+    is_complated : bool = Field(...,description='State of the Task')
+    
+    
+class TaskCreateSchemas (TaskBaseSchemas):
+    pass
+
+
+class TaskUpateSchemas(TaskBaseSchemas):
+    pass
+
+
+class TaskResponseSchemas(TaskBaseSchemas):
+    id :int = Field(...,description='unique identifier of the object')
+    creared_date : datetime =Field(...,description='Creations date and time of task')
+    updated_date : datetime =Field(...,description='Updating date and time of task')
