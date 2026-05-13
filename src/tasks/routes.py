7431f9d3ef4129070a0b73b33ed69acb137f2d12
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-05-13 13:14:57 +0330

    ** Refactored routes to show a list of tasks
    ++ Added limit value to limit the number of objects on the page for pagination
    
    ++Added offset for pagination and
    complicated to perform filtering operations based on true or false status

--- src/tasks/routes.py
+++ src/tasks/routes.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter,Path,Depends,HTTPException
+from fastapi import APIRouter,Path,Depends,HTTPException,Query
 from tasks.models import *
 from fastapi.responses import JSONResponse
 from tasks.schemas import *
@@ -9,9 +9,16 @@ from typing import List
 
 
 @router.get('/tasks/', response_model=List[TaskResponseSchemas])
-def get_all_tasks(db:Session=Depends(get_db)):
-    tasks =  db.query(TaskModels).all()
-    return tasks
+def get_all_tasks(db:Session=Depends(get_db),
+                  completed : bool = Query(None,description='filter tasks  based on being completed or not '),
+                  limit:int=Query(gt=0,le=50,description='limiting the count of task to retrive'),
+                  offset:int=Query(ge=0,deprecated='user for pagination base on passed item')):
+    query =  db.query(TaskModels)
+    if completed is not None:
+        query.filter_by( is_complated = completed)
+        return query 
+    tasks=query.limit(limit).offset(offset).all()
+    return query
 
 
 @router.post('/task/create')
