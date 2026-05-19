5613a9e2641cf300aa0debd2cc0a9cd7a68a5233
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-05-19 17:08:21 +0330

    Added Gzip middleware to compress requests larger than 1000 bytes File path changed: src/main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Request
 from contextlib import asynccontextmanager
 from tasks.routes import router as task_router
 from fastapi_swagger import patch_fastapi
 from users.routes import router as user_router
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
+import time
 @asynccontextmanager
 async def lifespan(app:FastAPI):
     yield
@@ -20,10 +22,15 @@ origins = [
     'http://127.0.0.1:3000',
     'http://localhost:3000']
 
-app.middleware(
+app.add_middleware(
     CORSMiddleware,
     allow_origins = origins,
     allow_credentials = True,
     allow_mehods = [''],
     allow_headers = [''] 
+)
+
+
+app.add_middleware(
+ GZipMiddleware,minimum_size=1000   
 )
