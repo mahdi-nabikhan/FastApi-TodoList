76c64beda5688430dfcd961e38e2ad42594be9b3
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-05-28 18:47:06 +0330

    add new config for  celery in celery_conf.py

--- src/core/celery_conf.py
+++ src/core/celery_conf.py
@@ -1,10 +1,24 @@
+
 from celery import Celery
 from core.config import setting
 
-celery_app=Celery('worker',broker=setting.CELERY_BROKER_URL,backend=setting.CELERY_BACKEND_URL)
+celery_app = Celery(
+    "worker",
+    broker=setting.CELERY_BROKER_URL,
+    backend=setting.CELERY_BACKEND_URL,
+)
 
+# Optional Celery Config
+celery_app.conf.update(
+    task_serializer="json",
+    accept_content=["json"],
+    result_serializer="json",
+    timezone="UTC",
+    enable_utc=True,
+)
 
 
 @celery_app.task
 def hello_world():
-    pass
+    return "Hello World"
+
