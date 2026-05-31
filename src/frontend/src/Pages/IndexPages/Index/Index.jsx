1afa8536b6da972471075f13714d1feccda9ddfe
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-05-31 18:11:13 +0330

    end fixing Updatetodo in index

--- src/frontend/src/Pages/IndexPages/Index/Index.jsx
+++ src/frontend/src/Pages/IndexPages/Index/Index.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import './Index.css'
 import DeleteModal from '../../../Components/DeleteModal/DeleteModal'
-
+import UpdateModal from '../../../Components/UpdateModal/UpdateModal'
 
 export default function Index() {
   const [todoList, setTodoList] = useState([])
   const [showModal, setShowModal] = useState(false)
   const [selectedTodo, setSelectedTodo] = useState(null)
+  const [showEditModal, setShowEditModal] = useState(false)
+
 
   function fetchTodos() {
     fetch('http://localhost:8000/tasks/?limit=10&offset=0', {
@@ -38,64 +40,100 @@ export default function Index() {
         setSelectedTodo(null)
       })
   }
+  const updateTodo = (updatedTodo) => {
+    fetch(`http://localhost:8000/task/${updatedTodo.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      credentials: 'include',
+      body: JSON.stringify(updatedTodo)
+    })
+      .then(res => res.json())
+      .then(data => {
+        setTodoList(prev =>
+          prev.map(item =>
+            item.id === updatedTodo.id ? data : item
+          )
+        )
+      })
+      .finally(() => {
+        setShowEditModal(false)
+        setSelectedTodo(null)
+      })
+  }
 
   return (
     <div className="table-container">
 
-  <h2>Todo List</h2>
+      <h2>Todo List</h2>
 
-  <table className="todo-table">
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>Title</th>
-        <th>Completed</th>
-        <th>Actions</th>
-      </tr>
-    </thead>
+      <table className="todo-table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Completed</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
 
-    <tbody>
-      {todoList.map(todo => (
-        <tr key={todo.id}>
-          <td>{todo.id}</td>
-          <td>{todo.title}</td>
-          <td>{todo.is_complated ? '✅' : '❌'}</td>
+        <tbody>
+          {todoList.map(todo => (
+            <tr key={todo.id}>
+              <td>{todo.id}</td>
+              <td>{todo.title}</td>
+              <td>{todo.is_complated ? '✅' : '❌'}</td>
 
-          <td>
-            <button className="btn details-btn">
-              Details
-            </button>
+              <td>
+                <button className="btn details-btn">
+                  Details
+                </button>
 
-            <button className="btn edit-btn">
-              Edit
-            </button>
+                <button
+                  className="btn edit-btn"
+                  onClick={() => {
+                    setSelectedTodo(todo)
+                    setShowEditModal(true)
+                  }}
+                >
+                  Edit
+                </button>
 
-            <button
-              className="btn delete-btn"
-              onClick={() => {
-                setSelectedTodo(todo)
-                setShowModal(true)
-              }}
-            >
-              Delete
-            </button>
-          </td>
-        </tr>
-      ))}
-    </tbody>
-  </table>
+                <button
+                  className="btn delete-btn"
+                  onClick={() => {
+                    setSelectedTodo(todo)
+                    setShowModal(true)
+                  }}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
 
-  <DeleteModal
-    isOpen={showModal}
-    todo={selectedTodo}
-    onClose={() => {
-      setShowModal(false)
-      setSelectedTodo(null)
-    }}
-    onConfirm={deleteTodo}
-  />
-
-</div>
+      <DeleteModal
+        isOpen={showModal}
+        todo={selectedTodo}
+        onClose={() => {
+          setShowModal(false)
+          setSelectedTodo(null)
+        }}
+        onConfirm={deleteTodo}
+      />
+      <UpdateModal
+        isOpen={showEditModal}
+        todo={selectedTodo}
+        onClose={() => {
+          setShowEditModal(false)
+          setSelectedTodo(null)
+        }}
+        onSave={updateTodo}
+      />
+    </div>
   )
 }
 
