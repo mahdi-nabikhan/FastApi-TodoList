ad08a2e301be130da8556bf162e3ffc780a77d3e
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-06-01 09:07:50 +0330

    feat(frontend):add new component for showing deatil of a Todo named TodoDeatilModal

---
+++ src/frontend/src/Components/TodoDeatilModal/TodoDeatilModal.jsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import './TodoDetailsModal.css'
+
+export default function TodoDetailsModal({
+  isOpen,
+  onClose,
+  todo
+}) {
+  if (!isOpen || !todo) return null
+
+  return (
+    <div className="modal-overlay">
+      <div className="modal">
+
+        <h2>Todo Details</h2>
+
+        <div className="detail-item">
+          <strong>ID:</strong>
+          <span>{todo.id}</span>
+        </div>
+
+        <div className="detail-item">
+          <strong>Title:</strong>
+          <span>{todo.title}</span>
+        </div>
+
+        <div className="detail-item">
+          <strong>Description:</strong>
+          <span>{todo.description}</span>
+        </div>
+
+        <div className="detail-item">
+          <strong>Status:</strong>
+          <span>
+            {todo.is_complated ? '✅ Completed' : '❌ Not Completed'}
+          </span>
+        </div>
+
+        <div className="detail-item">
+          <strong>Created:</strong>
+          <span>{todo.created_date}</span>
+        </div>
+
+        <div className="detail-item">
+          <strong>Updated:</strong>
+          <span>{todo.updated_date}</span>
+        </div>
+
+        <div className="modal-actions">
+          <button
+            className="close-btn"
+            onClick={onClose}
+          >
+            Close
+          </button>
+        </div>
+
+      </div>
+    </div>
+  )
+}
