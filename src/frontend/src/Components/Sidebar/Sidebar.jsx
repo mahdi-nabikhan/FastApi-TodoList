815b8946ab05ff2f6b3b47c232c22978a18a900d
mahdi-nabikhan <156818753+mahdi-nabikhan@users.noreply.github.com>
2026-06-01 11:14:22 +0330

    feat(frontend):add side bar component with template and style for panel admin feat(fronte):change layout of panel admin and add side bar

---
+++ src/frontend/src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Sidebar.css'
+import { Link } from 'react-router'
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+        <div className="sidebarWrapper">
+            <div className="sidebarMenu">
+                <h3 className="sidebarTitle">Dashbord</h3>
+                <ul className="sidebarList">
+                    <Link className='text-link' to={'/'}><li className="sidebarListItem active">Home</li></Link>
+                    <Link className='text-link' to={'/'}><li className="sidebarListItem active">Home</li></Link>
+                    <Link className='text-link' to={'/'}><li className="sidebarListItem active">Home</li></Link>
+                </ul>
+            </div>
+        </div>
+    </div>
+  )
+}
--- src/frontend/src/layouts/PanelLayout.jsx
+++ src/frontend/src/layouts/PanelLayout.jsx
@@ -3,15 +3,21 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import PanelNavbar from '../Components/PanelNavbar/PanelNavbar'
+import Sidebar from '../Components/Sidebar/Sidebar'
+import './PanelLayout.css'
 
 export default function PanelLayout() {
   return (
     <>
       <PanelNavbar />
+      <div className="container">
+        <Sidebar/>
+        <main>
+          <Outlet />
+        </main>
 
-      <main>
-        <Outlet />
-      </main>
+      </div>
+      
     </>
   )
 }
